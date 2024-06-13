5d1ae1a6bd34fae2f8a95c74c11b74d9141a074e
jeferson <jeferson9488@gmail.com>
2024-06-12 23:46:24 -0300

    Replicando desafio

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        for(int i = 1; i <= 5; i++){
-            System.out.println("i = " + i);
-        }
-    }
-}
